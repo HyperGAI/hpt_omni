9426776697b0a913a195dab9084e9fc216c879db
yangwenz08 <wzyang@hypergai.com>
2024-06-25 17:53:33 +0800

    Tests for supervised datasets

--- tests/datasets/test_supervised_dataset.py
+++ tests/datasets/test_supervised_dataset.py
@@ -1,12 +1,15 @@
 import unittest
 from transformers import AutoTokenizer
+from llava import conversation as conversation_lib
 from llava.data.dataset import LazySupervisedDataset
 from llava.train.args import DataArguments
+from llava.model.multimodal_encoder.intern_encoder import InternVisionPreprocessor
 
 
 class TestSupervisedDataset(unittest.TestCase):
 
     def test(self):
+        conversation_lib.default_conversation = conversation_lib.conv_templates["llama_3"]
         tokenizer = AutoTokenizer.from_pretrained(
             "/export/share/models/Meta-Llama-3-8B-Instruct/",
             model_max_length=10000,
@@ -14,11 +17,16 @@ class TestSupervisedDataset(unittest.TestCase):
             use_fast=False,
             legacy=False,
         )
+        tokenizer.pad_token_id = -1
         data_args = DataArguments(
             data_path="/export/share/dataset/LLaVA-Pretrain/blip_laion_cc_sbu_558k.json",
             image_folder="/export/share/dataset/LLaVA-Pretrain/images",
             data_mixture="blip_laion_cc_sbu_558k"
         )
+        data_args.image_processor = InternVisionPreprocessor()
+        data_args.is_multimodal = True
+        data_args.mm_use_im_start_end = False
+
         dataset = LazySupervisedDataset(
             data_path="/export/share/dataset/LLaVA-Pretrain/blip_laion_cc_sbu_558k.json",
             image_folder="/export/share/dataset/LLaVA-Pretrain/images",
